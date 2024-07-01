d27a4bebaa3433583151bf01ab7f0775c1edfa65
gkibria121 <gkibria121@gmail.com>
2024-07-01 22:27:59 +0600

    wip

--- test.py
+++ test.py
@@ -1,80 +1,107 @@
+import asyncio
+import nodriver as uc
 import time
-from random import randint
-from fake_useragent import UserAgent
-from seleniumbase import Driver
-from selenium_stealth import stealth
-from bs4 import BeautifulSoup
-import json
-class Browser:
-    def __init__(self,proxy):
-        self.driver = None
-        self.proxy = proxy
 
-    def get_random_user_agent(self):
-        ua = UserAgent()
-        while True:
-            random_user_agent = ua.chrome
-            if "Windows" in random_user_agent:
-                return random_user_agent
+PROXY = "serv.dtt360.com:8000"
+USERNAME = "Skhan"
+PASSWORD = "qGsg86afVQOnK-country-US-session-7Xx9B3Pb"
 
-    def setup_driver(self):
-        proxy_host = self.proxy
-        args = """--lang=en-US,
-            --disable-web-security,
-            --disable-gpu,
-            --no-sandbox,
-            --disable-dev-shm-usage,
-            --disable-features=NetworkService,
-            --disable-features=VizDisplayCompositor,
-            --disable-software-rasterize"""
-        random_user_agent = self.get_random_user_agent()
+class Scraper:
+    main_tab: uc.Tab
+
+    def __init__(self):
+        uc.loop().run_until_complete(self.run())
+
+    async def run(self):
+        browser = await uc.start(
+            browser_args=[
+                f"--proxy-server={PROXY}",
+                "--disable-webrtc",
+                "--webrtc.ip_handling_policy=disable_non_proxied_udp",
+                "--webrtc.multiple_routes_enabled=false",
+                "--webrtc.nonproxied_udp_enabled=false",
+                # Additional flags to disable proxy detection
+                "--disable-extensions",
+                "--disable-gpu",
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-accelerated-2d-canvas",
+                "--no-first-run",
+                "--no-zygote",
+                "--disable-remote-fonts",
+                "--disable-client-side-phishing-detection",
+                "--disable-ipc-flooding-protection",
+                "--disable-popup-blocking",
+                "--disable-prompt-on-repost",
+                "--ignore-certificate-errors",
+                "--no-default-browser-check",
+                "--disable-infobars",
+                "--disable-breakpad",
+                "--disable-cast",
+                "--disable-cast-streaming-hw-encoding",
+                "--disable-cloud-import",
+                "--disable-default-apps",
+                "--disable-extensions-file-access-check",
+                "--disable-notifications",
+                "--disable-password-generation",
+                "--disable-print-preview",
+                "--disable-speech-api",
+                "--disable-voice-input",
+                "--disable-wake-on-wifi",
+                "--hide-scrollbars",
+                "--metrics-recording-only",
+                "--mute-audio",
+                "--no-pings",
+                "--password-store=basic",
+                "--use-mock-keychain",
+                "--disable-blink-features=AutomationControlled",
+            ],
+        )
+        self.main_tab = await browser.get("draft:,")
+        self.main_tab.add_handler(uc.cdp.fetch.RequestPaused, self.req_paused)
+        self.main_tab.add_handler(
+            uc.cdp.fetch.AuthRequired, self.auth_challenge_handler
+        )
+        await self.main_tab.send(uc.cdp.fetch.enable(handle_auth_requests=True))
         
-        self.driver = Driver(
-            uc=True, headless=False, uc_subprocess=True, uc_cdp_events=True,
-            uc_cdp=True, log_cdp=True, log_cdp_events=True, undetectable=True,
-            chromium_arg=args, swiftshader=True, agent=random_user_agent, proxy=proxy_host
+        # Additional steps to evade detection
+        await self.set_webdriver(self.main_tab)
+        
+        page = await browser.get("https://www.browserscan.net")
+        await asyncio.sleep(6000)
+
+    async def auth_challenge_handler(self, event: uc.cdp.fetch.AuthRequired):
+        asyncio.create_task(
+            self.main_tab.send(
+                uc.cdp.fetch.continue_with_auth(
+                    request_id=event.request_id,
+                    auth_challenge_response=uc.cdp.fetch.AuthChallengeResponse(
+                        response="ProvideCredentials",
+                        username=USERNAME,
+                        password=PASSWORD,
+                    ),
+                )
+            )
         )
-        stealth(
-            self.driver,
-            languages=["en-US", "en"],
-            vendor="Google Inc.",
-            platform="Win32",
-            webgl_vendor="Intel Inc.",
-            renderer="Intel Iris OpenGL Engine",
-            fix_hairline=True
+
+    async def req_paused(self, event: uc.cdp.fetch.RequestPaused):
+        asyncio.create_task(
+            self.main_tab.send(
+                uc.cdp.fetch.continue_request(request_id=event.request_id)
+            )
         )
-        self.update_time_zone()
-        # self.driver.set_window_rect(randint(4, 720), randint(8, 410), 700, 900)
-    def update_time_zone(self):
-        self.driver.get("https://ipinfo.io/json")
-        page_source = self.driver.page_source
-        soup = BeautifulSoup(page_source, 'html.parser')
-        pre_tag =soup.find("pre")
-        ip_info = json.loads(pre_tag.get_text())
-         
-        print(ip_info)
-        tz_params = {'timezoneId': ip_info['timezone']}
-        time_zone = ip_info['timezone']  # Example: Eastern Time (US & Canada)
-        script = f"Intl.DateTimeFormat().resolvedOptions().timeZone = '{time_zone}';"
-        self.driver.execute_script(script)
 
-    def open_browserscan(self):
-        try:
-            self.setup_driver()
-            apple_registration_url = "https://www.browserscan.net/"
-            start_time = time.time()
-            self.driver.uc_open_with_reconnect(apple_registration_url, reconnect_time=10)
-            stop_time = time.time()
-            print("SeleniumBase time:", stop_time - start_time)
-            time.sleep(60)
+    async def set_webdriver(self, tab):
+        await tab.send(
+            uc.cdp.page.add_script_to_evaluate_on_new_document(
+                source="""
+                Object.defineProperty(navigator, 'webdriver', {
+                    get: () => undefined
+                })
+                """
+            )
+        )
 
-        except Exception as e:
-            print(f"Error: {e}")
-
-        finally:
-            if self.driver:
-                self.driver.quit()
-
-if __name__ == '__main__':
-    browser = Browser('Skhan:qGsg86afVQOnK@serv.dtt360.com:8000')
-    browser.open_browserscan()
+if __name__ == "__main__":
+    Scraper()
